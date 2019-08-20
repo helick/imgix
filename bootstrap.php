7775b56e507c244b4ed5baffbaf3b09ef0a80d8a
Nasyrov Evgenii <inasyrov@ya.ru>
2019-08-20 20:12:14 +0100

    feat(plugin): add imgix url helper

--- bootstrap.php
+++ bootstrap.php
@@ -12,5 +12,6 @@ if (file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
 
 // Constants & helpers
 require_once __DIR__ . '/src/constants.php';
+require_once __DIR__ . '/src/helpers.php';
 
 // Services
---
+++ src/helpers.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Helick\Imgix;
+
+use Imgix\UrlBuilder;
+
+/**
+ * Get the imgix domain.
+ *
+ * @return string
+ */
+function domain(): string
+{
+    return defined('IMGIX_DOMAIN')
+        ? IMGIX_DOMAIN
+        : '';
+}
+
+/**
+ * Get the imgix url.
+ *
+ * @param string $imageUrl
+ * @param array  $args
+ * @param bool   $useHttps
+ * @param string $signKey
+ *
+ * @return string
+ */
+function url(string $imageUrl, array $args = [], bool $useHttps = true, string $signKey = ''): string
+{
+    $requiresProcessing = strpos($imageUrl, wp_upload_dir()['baseurl']) === 0;
+
+    /**
+     * Control whether the given image url is processable.
+     *
+     * @param bool   $isProcessable
+     * @param string $imageUrl
+     * @param array  $args
+     */
+    $isProcessable = apply_filters('imgix_image_url_processable', true, $imageUrl, $args);
+    $isProcessable = (bool)$isProcessable;
+
+    if (!$requiresProcessing || !$isProcessable) {
+        return $imageUrl;
+    }
+
+    $imageUrlPath = parse_url($imageUrl, PHP_URL_PATH);
+
+    $imageFile = basename($imageUrlPath);
+    $imageFile = urlencode($imageFile);
+
+    $imageUrlPath = str_replace($imageUrl, $imageFile, $imageUrlPath);
+
+    /**
+     * Control the imgix image url path.
+     *
+     * @param string $imageUrlPath
+     * @param array  $args
+     */
+    $imageUrlPath = apply_filters('imgix_image_url_path', $imageUrlPath, $args);
+
+    /**
+     * Control the imgix arguments.
+     *
+     * @param array  $args
+     * @param string $imageUrlPath
+     */
+    $args = apply_filters('imgix_args', $args, $imageUrlPath);
+
+    $urlBuilder = new UrlBuilder(domain(), $useHttps, $signKey);
+    $imgixUrl   = $urlBuilder->createURL($imageUrlPath, $args);
+
+    /**
+     * Control the imgix url.
+     *
+     * @param string $imgixUrl
+     * @param string $imageUrl
+     * @param array  $args
+     */
+    $imgixUrl = apply_filters('imgix_url', $imgixUrl, $imageUrl, $args);
+
+    return $imgixUrl;
+}
